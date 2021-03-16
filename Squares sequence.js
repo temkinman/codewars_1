e9b62e594a8d5fd91a2a12ed2c59b30dd1789838
temkinman <temkinman@mail.ru>
2021-03-16 20:59:18 +0300

    first task from Number solved

---
+++ Squares sequence.js
@@ -0,0 +1,39 @@
+/*
+Complete the function that returns an array of length n, starting with the given number x and the squares of the previous number. If n is negative or zero, return an empty array/list.
+
+Examples
+2, 5  -->  [2, 4, 16, 256, 65536]
+3, 3  -->  [3, 9, 81]
+*/
+
+function squares(x, n) {
+  if (x < 0 || n < 0) {
+    return [];
+  }
+  let result = [x];
+  for (let i = 1; i < n; i++) {
+    result.push(Math.pow(result[i - 1], 2));
+  }
+  return result;
+}
+
+/*
+  function squares(x, n) {
+  let arr = [];
+  if(n === 0 || n < 0) return arr;
+  for(let i = x; arr.length < n; i*=i) {
+      arr.push(i);
+  }
+  return arr
+}
+
+function squares(x, n) {
+  var ret = [];
+  for(var i=0; i<n; i++){
+    ret.push(x);
+    x*= x;
+  }
+  return ret;
+}
+  */
+console.log(squares(2, 5));
